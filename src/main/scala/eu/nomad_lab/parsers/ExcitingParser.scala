bb6ecf12dfa597711c08d47beaffe0328880f645
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-02-04 16:12:53 +0100

    Scala Integration

---
+++ src/main/scala/eu/nomad_lab/parsers/ExcitingParser.scala
@@ -0,0 +1,26 @@
+package eu.nomad_lab.parsers
+import eu.nomad_lab.DefaultPythonInterpreter
+import org.{json4s => jn}
+
+object ExcitingParser extends SimpleExternalParserGenerator(
+  name = "ExcitingParser",
+  parserInfo = jn.JObject(
+    ("name" -> jn.JString("ExcitingParser")) ::
+      ("version" -> jn.JString("1.0")) :: Nil),
+  mainFileTypes = Seq("text/.*"),
+  mainFileRe = """\s*Invoking exciting \.\.\.
+\s*Version """.r,
+  cmd = Seq(DefaultPythonInterpreter.python2Exe(), "${envDir}/parsers/exciting/parser/parser-exciting/parser_exciting.py",
+    "--uri", "${mainFileUri}", "${mainFilePath}"),
+  resList = Seq(
+    "parser-exciting/parser_exciting.py",
+    "parser-exciting/setup_paths.py",
+    "nomad_meta_info/common.nomadmetainfo.json",
+    "nomad_meta_info/meta_types.nomadmetainfo.json",
+    "nomad_meta_info/exciting.nomadmetainfo.json"
+  ) ++ DefaultPythonInterpreter.commonFiles(),
+  dirMap = Map(
+    "parser-exciting" -> "parsers/exciting/parser/parser-exciting/",
+    "nomad_meta_info" -> "nomad-meta-info/meta_info/nomad_meta_info",
+    "python" -> "python-common/common/python/nomadcore") ++ DefaultPythonInterpreter.commonDirMapping()
+)
