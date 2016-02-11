af23df6a97c09d71aa21aeea36f09c98f68ae081
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-02-11 10:05:50 +0100

    Add test; Warning no real files to test are present in the directory at the moment

---
+++ src/test/scala/eu/nomad_lab/parsers/ExcitingParserSpec.scala
@@ -0,0 +1,18 @@
+package eu.nomad_lab.parsers
+
+import org.specs2.mutable.Specification
+
+
+
+object ExcitingParserSpec extends Specification {
+  "ExcitingParserTest" >> {
+    "test with json-events" >>  {
+      //No test file present at the moment; Replace the README when test file is present
+      ParserRun.parse(ExcitingParser,"parsers/castep/test/examples/README.md","json-events") must_== ParseResult.ParseSuccess
+    }
+  }
+
+  "test with json" >>  {
+    ParserRun.parse(ExcitingParser,"parsers/exciting/test/examples/README.md","json") must_== ParseResult.ParseSuccess
+  }
+}
