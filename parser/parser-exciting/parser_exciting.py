4a872f173df49fb8dbed7fe2fd89d6468c687bdc
lopa <pardini@physik.hu-berlin.de>
2016-02-12 16:41:07 +0100

    parse total and scf energy and add test files

--- parser/parser-exciting/parser_exciting.py
+++ parser/parser-exciting/parser_exciting.py
@@ -1,57 +1,96 @@
 import setup_paths
-from nomadcore.simple_parser import mainFunction, SimpleMatcher as SM
+import numpy as np
+from nomadcore.simple_parser import SimpleMatcher as SM, mainFunction
 from nomadcore.local_meta_info import loadJsonFile, InfoKindEl
+from nomadcore.caching_backend import CachingLevel
 import os, sys, json
 
-# description of the input
-mainFileDescription = SM(
-    name = 'root',
-    weak = True,
-    startReStr = "",
-    subMatchers = [
-        SM(name = 'newRun',
-           startReStr = r"\s*# SampleParser #\s*",
-           repeats = True,
-           required = True,
-           forwardMatch = True,
-           sections   = ['section_run'],
-           subMatchers = [
-               SM(name = 'header',
-                  startReStr = r"\s*# SampleParser #\s*")
-           ])
-])
+class ExcitingParserContext(object):
 
-# loading metadata from nomad-meta-info/meta_info/nomad_meta_info/exciting.nomadmetainfo.json
-metaInfoPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../../../../nomad-meta-info/meta_info/nomad_meta_info/exciting.nomadmetainfo.json"))
-metaInfoEnv, warnings = loadJsonFile(filePath = metaInfoPath, dependencyLoader = None, extraArgsHandling = InfoKindEl.ADD_EXTRA_ARGS, uri = None)
+  def startedParsing(self, path, parser):
+    self.parser=parser
+
+  def onClose_exciting_section_lattice_vectors(self, backend, gIndex, section):
+    latticeX = section["exciting_geometry_lattice_vector_x"]
+    latticeY = section["exciting_geometry_lattice_vector_y"]
+    latticeZ = section["exciting_geometry_lattice_vector_z"]
+#    print "section", section.simpleValues
+#   print "latticeX",latticeX
+#   print "latticeY",latticeY
+#    print "latticeZ",latticeZ 
+    cell = [[latticeX[0],latticeY[0],latticeZ[0]],
+            [latticeX[1],latticeY[1],latticeZ[1]],
+            [latticeX[2],latticeY[2],latticeZ[2]]]
+    backend.addValue("simulation_cell", cell)
+
+# description of the input
+
+
+
+
+mainFileDescription = \
+    SM(name = "root matcher",
+       startReStr = "",
+       weak = True,
+       subMatchers = [
+         SM(name = "header",
+         startReStr = r"\s*\|\s*EXCITING\s*(?P<program_version>[-a-zA-Z0-9]+)\s*started\s*=",
+         sections = ["section_run"],
+         subMatchers = [
+	   SM(name = 'input',
+              startReStr = r"\|\sStarting initialization",
+              endReStr = r"\|\sEnding initialization",
+              sections = ['section_system_description'],
+              subMatchers = [
+                SM(startReStr = r"\sLattice vectors \(cartesian\) :",
+                sections = ["exciting_section_lattice_vectors"],
+                subMatchers = [
+
+    SM(startReStr = r"\s*(?P<exciting_geometry_lattice_vector_x__bohr>[-+0-9.]+)\s+(?P<exciting_geometry_lattice_vector_y__bohr>[-+0-9.]+)\s+(?P<exciting_geometry_lattice_vector_z__bohr>[-+0-9.]+)", repeats = True)
+                ])
+              ]),
+            SM(name = "single configuration iteration",
+              startReStr = r"\|\s*Self-consistent loop started\s*\+",
+              sections = ["section_single_configuration_calculation"],
+              repeats = True,
+              subMatchers = [
+                SM(name = "scfi totE",
+                 startReStr =r"\|\s*SCF iteration number\s*:",
+                  sections = ["section_scf_iteration"],
+                  repeats = True,
+                  subMatchers = [
+                   SM(r"\s*Total energy\s*:\s*(?P<energy_total_scf_iteration__hartree>[-0-9.]+)")
+                  ]),
+                SM(name="final_energy",
+                  startReStr = r"\| Convergence targets achieved. Performing final SCF iteration\s*\+",
+                  endReStr = r"\| Self-consistent loop stopped\s*\+",
+                   subMatchers = [
+                     SM(r"\s*Total energy\s*:\s*(?P<energy_total__hartree>[-0-9.]+)")
+                   ])
+               ]
+            )
+          ])
+    ])
+
+
+
 
 parserInfo = {
-  "name": "parser_exciting",
+  "name": "exciting_parser",
   "version": "1.0"
 }
 
-class ExcitingParserContext(object):
-    """main place to keep the parser status, open ancillary files,..."""
-    def __init__(self):
-        self.scfIterNr = 0
+metaInfoPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../../../../nomad-meta-info/meta_info/nomad_meta_info/exciting.nomadmetainfo.json"))
+metaInfoEnv, warnings = loadJsonFile(filePath = metaInfoPath, dependencyLoader = None, extraArgsHandling = InfoKindEl.ADD_EXTRA_ARGS, uri = None)
 
-    # just examples, you probably want to remove the following two triggers
-
-    def onClose_section_single_configuration_calculation(self, backend, gIndex, section):
-        """trigger called when section_single_configuration_calculation is closed"""
-        #backend.addValue("", self.scfIterNr)
-        logging.getLogger("nomadcore.parsing").info("closing section_single_configuration_calculation gIndex %d %s", gIndex, section.simpleValues)
-        self.scfIterNr = 0
-
-    def onClose_section_scf_iteration(self, backend, gIndex, section):
-        """trigger called when section_scf_iteration is closed"""
-        logging.getLogger("nomadcore.parsing").info("closing section_scf_iteration bla gIndex %d %s", gIndex, section.simpleValues)
-        self.scfIterNr += 1
-
-# which values to cache or forward (mapping meta name -> CachingLevel)
-cachingLevelForMetaName = {}
+cachingLevelForMetaName = {
+                            "exciting_geometry_lattice_vector_x":CachingLevel.Cache,
+                            "exciting_geometry_lattice_vector_y":CachingLevel.Cache,
+                            "exciting_geometry_lattice_vector_z":CachingLevel.Cache,
+                            "exciting_section_lattice_vectors": CachingLevel.Ignore
+                          }
 
 if __name__ == "__main__":
-    mainFunction(mainFileDescription, metaInfoEnv, parserInfo,
-                 cachingLevelForMetaName = cachingLevelForMetaName,
-                 superContext = ExcitingParserContext())
+#    for name in metaInfoEnv.infoKinds:
+#        print 'nameo', name
+    mainFunction(mainFileDescription, metaInfoEnv, parserInfo, cachingLevelForMetaName = cachingLevelForMetaName, superContext=ExcitingParserContext())
