7252d0b25ef33c5b19e0c4f881c0ca2ce027dad7
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-04-08 17:20:55 +0200

    scalariform

--- src/test/scala/eu/nomad_lab/parsers/ExcitingParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/ExcitingParserSpec.scala
@@ -2,17 +2,15 @@ package eu.nomad_lab.parsers
 
 import org.specs2.mutable.Specification
 
-
-
 object ExcitingParserSpec extends Specification {
   "ExcitingParserTest" >> {
-    "test with json-events" >>  {
+    "test with json-events" >> {
       //No test file present at the moment; Replace the README when test file is present
-      ParserRun.parse(ExcitingParser,"parsers/castep/test/examples/README.md","json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(ExcitingParser, "parsers/castep/test/examples/README.md", "json-events") must_== ParseResult.ParseSuccess
     }
   }
 
-  "test with json" >>  {
-    ParserRun.parse(ExcitingParser,"parsers/exciting/test/examples/README.md","json") must_== ParseResult.ParseSuccess
+  "test with json" >> {
+    ParserRun.parse(ExcitingParser, "parsers/exciting/test/examples/README.md", "json") must_== ParseResult.ParseSuccess
   }
 }
