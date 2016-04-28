0c2b53ae4a450adea6fb445c7090fe37c4021503
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-04-28 17:09:40 +0200

    Add nomad meta info version and update ci script

--- src/main/scala/eu/nomad_lab/parsers/ExcitingParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/ExcitingParser.scala
@@ -11,7 +11,9 @@ object ExcitingParser extends SimpleExternalParserGenerator(
     ("name" -> jn.JString("ExcitingParser")) ::
       ("parserId" -> jn.JString("ExcitingParser" + lab.ExcitingVersionInfo.version)) ::
       ("versionInfo" -> jn.JObject(
-        ("nomadCoreVersion" -> jn.JString(lab.NomadCoreVersionInfo.version)) ::
+        ("nomadCoreVersion" -> jn.JObject(lab.NomadCoreVersionInfo.toMap.map {
+          case (k, v) => k -> jn.JString(v.toString)
+        }(breakOut): List[(String, jn.JString)])) ::
           (lab.ExcitingVersionInfo.toMap.map {
             case (key, value) =>
               (key -> jn.JString(value.toString))
