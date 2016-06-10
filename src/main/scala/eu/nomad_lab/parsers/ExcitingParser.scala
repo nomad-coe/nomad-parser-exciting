053b8f371e40371c08144d40e3a94d62d1811815
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2016-06-10 12:34:39 +0200

    adding exciting_parser_bandstructure.py to scala wrapper

--- src/main/scala/eu/nomad_lab/parsers/ExcitingParser.scala
+++ src/main/scala/eu/nomad_lab/parsers/ExcitingParser.scala
@@ -29,6 +29,7 @@ object ExcitingParser extends SimpleExternalParserGenerator(
   resList = Seq(
     "parser-exciting/parser_exciting.py",
     "parser-exciting/exciting_parser_dos.py",
+    "parser-exciting/exciting_parser_bandstructure.py",
     "parser-exciting/setup_paths.py",
     "nomad_meta_info/public.nomadmetainfo.json",
     "nomad_meta_info/common.nomadmetainfo.json",
