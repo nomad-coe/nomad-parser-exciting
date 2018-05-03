4d0f825e984a874e6e374c1de8b2f9d972c19b59
lopa <pardini@physik.hu-berlin.de>
2018-05-03 14:49:07 +0200

    adapted headers

--- parser/parser-exciting/exciting_parser_input.py
+++ parser/parser-exciting/exciting_parser_input.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 The NOMAD Developers Group
+# Copyright 2017-2018 Lorenzo Pardini
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Main author and maintainer: Lorenzo Pardini <loren.pard@gmail.com>
+
 import xml.sax
 import logging
 import numpy as np
