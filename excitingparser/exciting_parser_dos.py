Fixed normalization issue in DOS values, removed the output of normalized dos energies.

excitingparser/exciting_parser_dos.py
@@ -116,20 +116,17 @@ class DosHandler(xml.sax.handler.ContentHandler):
                 self.totDosSpin[1] = self.totDos[0:self.numDosVal]
                 self.energySpin = self.energy[0:self.numDosVal]
                 self.backend.addValue("dos_values", np.array(self.totDosSpin) * self.unitCellVol)
-                self.backend.addValue("dos_energies",self.energySpin)
+                self.backend.addValue("dos_energies", self.energySpin + self.fermiEnergy)
                 self.backend.addValue("number_of_dos_values", self.numDosVal)
-                self.backend.addValue("dos_kind","electronic")
+                self.backend.addValue("dos_kind", "electronic")
             else:
-                self.numDosVal = int(len(self.energy)/2)
+                self.numDosVal = int(len(self.energy) / 2)
                 self.totDosSpin[0] = self.totDos[0:self.numDosVal]
-                self.totDosSpin[1] = self.totDos[self.numDosVal:int(2*(self.numDosVal))]
+                self.totDosSpin[1] = self.totDos[self.numDosVal:int(2 * (self.numDosVal))]
                 self.energySpin = self.energy[0:self.numDosVal]
                 self.backend.addValue("dos_values", np.array(self.totDosSpin) * self.unitCellVol)
-                self.backend.addValue("dos_energies",self.energySpin)
+                self.backend.addValue("dos_energies", self.energySpin + self.fermiEnergy)
                 self.backend.addValue("number_of_dos_values", self.numDosVal)
-
-            if self.fermiEnergy is not None:
-                self.backend.addValue("dos_energies_normalized", self.energySpin - self.fermiEnergy)
 
         elif name == 'partialdos':
             pass